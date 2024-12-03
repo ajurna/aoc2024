5c9a17aa0a1c483a00a12aa5c664e07a880387f5
Ajurna <ajurna@gmail.com>
2024-12-03 11:34:19 +0000

    init

---
+++ template.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def part1(lines: List[str]):
+    pass
+
+def part2(lines: List[str]):
+    pass
+
+if __name__ == '__main__':
+    with open("input") as f:
+        lines = f.readlines()
+    part1(lines)
+    part2(lines)
